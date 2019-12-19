625e507cdebc79c89d95e97d84b4c2cb4de9f2fd
Andrej Babic <andrej.babic@psi.ch>
2019-12-19 19:58:12 +0100

    Merge the implementation and header

--- src/ChannelValue.h
+++ src/ChannelValue.h
@@ -19,14 +19,24 @@ namespace bs_daq {
         const size_t data_n_bytes_;
         const std::unique_ptr<char> buffer_;
 
-        ChannelValue(std::string channel_name,
-                     int64_t pulse_id_mod,
-                     int64_t pulse_id,
-                     std::string type,
-                     const std::vector<uint32_t>& shape,
-                     std::string encoding,
-                     std::string compression,
-                     size_t data_n_bytes);
+        ChannelValue(
+                std::string channel_name,
+                int64_t pulse_id_mod,
+                int64_t pulse_id,
+                std::string type,
+                const std::vector<uint32_t>& shape,
+                std::string encoding,
+                std::string compression,
+                size_t data_n_bytes)
+                : channel_name_(channel_name),
+                  pulse_id_mod_(pulse_id_mod),
+                  pulse_id_(pulse_id),
+                  type_(type),
+                  shape_(shape),
+                  encoding_(encoding),
+                  compression_(compression),
+                  data_n_bytes_(data_n_bytes),
+                  buffer_(std::make_unique<char>(data_n_bytes)) {}
     };
 
 }
