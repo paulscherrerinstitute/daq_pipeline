2e6d28a348d3e6b0bb81011d6fca8a1672fcde0e
Andrej Babic <andrej.babic@psi.ch>
2019-12-19 19:21:37 +0100

    Reconstruct channel value class

--- src/ChannelValue.cpp
+++ src/ChannelValue.cpp
@@ -8,7 +8,7 @@ bs_daq::ChannelValue::ChannelValue(
         const std::vector<uint32_t>& shape,
         std::string encoding,
         std::string compression,
-        size_t max_data_n_bytes)
+        size_t data_n_bytes)
             : channel_name_(channel_name),
               pulse_id_mod_(pulse_id_mod),
               pulse_id_(pulse_id),
@@ -16,15 +16,5 @@ bs_daq::ChannelValue::ChannelValue(
               shape_(shape),
               encoding_(encoding),
               compression_(compression),
-              max_data_n_bytes_(max_data_n_bytes),
-              buffer_(std::make_unique<char>(max_data_n_bytes_)),
-              data_n_bytes_(0) {}
-
-
-void bs_daq::ChannelValue::set_data_n_bytes(size_t data_n_bytes) {
-    data_n_bytes_ = data_n_bytes;
-}
-
-size_t bs_daq::ChannelValue::get_data_n_bytes() {
-    return data_n_bytes_;
-}
+              data_n_bytes_(data_n_bytes),
+              buffer_(std::make_unique<char>(data_n_bytes)) {}
--- src/ChannelValue.h
+++ src/ChannelValue.h
@@ -7,12 +7,7 @@
 
 namespace bs_daq {
 
-    class ChannelValue {
-
-    private:
-        size_t data_n_bytes_;
-
-    public:
+    struct ChannelValue {
 
         const std::string channel_name_;
         const int64_t pulse_id_mod_;
@@ -21,7 +16,7 @@ namespace bs_daq {
         const std::vector<uint32_t> shape_;
         const std::string encoding_;
         const std::string compression_;
-        const size_t max_data_n_bytes_;
+        const size_t data_n_bytes_;
         const std::unique_ptr<char> buffer_;
 
         ChannelValue(std::string channel_name,
@@ -31,11 +26,7 @@ namespace bs_daq {
                      const std::vector<uint32_t>& shape,
                      std::string encoding,
                      std::string compression,
-                     size_t max_data_n_bytes);
-
-        void set_data_n_bytes(size_t data_n_bytes);
-        size_t get_data_n_bytes();
-
+                     size_t data_n_bytes);
     };
 
 }
