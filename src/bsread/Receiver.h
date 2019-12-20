199e21d4f13e13f8a7adedcfc226b9f46d2c9443
Andrej Babic <andrej.babic@psi.ch>
2019-12-20 12:20:15 +0100

    Remove unused structs

--- src/bsread/Receiver.h
+++ src/bsread/Receiver.h
@@ -12,35 +12,6 @@
 
 namespace bsread {
 
-    struct data_channel_value {
-
-        data_channel_value() : data(nullptr), data_len(0) {}
-
-        data_channel_value(char* data, size_t data_len) :
-                data(data, std::default_delete<char[]>()),
-                data_len(data_len)
-        {}
-
-        std::shared_ptr<char> data;
-        size_t data_len;
-        std::shared_ptr<timestamp> timestamp;
-    };
-
-    struct data_channel_header {
-        std::string name;
-        bsdata_type type;
-        std::vector<uint32_t> shape;
-        compression_type compression;
-        endianess endianess;
-        uint32_t modulo;
-        uint32_t offset;
-    };
-
-    struct data_header {
-        std::string htype;
-        std::vector<data_channel_header> channels;
-    };
-
     struct main_header {
         std::string htype;
         uint64_t pulse_id;
@@ -68,7 +39,7 @@ namespace bsread {
         data_channel_value get_channel_data(void* data, size_t data_len, compression_type compression);
         std::shared_ptr<timestamp> get_channel_timestamp(void* data, size_t data_len);
 
-        std::string current_data_header_hash_;
+        std::string channels_data_hash_;
         std::vector<std::unique_ptr<ChannelData>> channels_data_;
     };
 }
