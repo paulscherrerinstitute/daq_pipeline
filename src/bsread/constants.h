73b995430b7cea3ff4e7d1f2d08279f7d315d1fe
Andrej Babic <andrej.babic@psi.ch>
2019-12-20 12:40:08 +0100

    Compact constants

--- src/bsread/constants.h
+++ src/bsread/constants.h
@@ -6,12 +6,6 @@
 
 namespace bsread {
 
-    // 100 characters for 6 attributes
-    static const size_t MAX_HEADER_LEN = 100 * 6;
-
-    // 200 characters for 200 channels.
-    static const size_t MAX_DATA_HEADER_LEN = 200 * 200;
-
     struct timestamp{
 
         timestamp(void): sec(0), nsec(0) {};
@@ -28,62 +22,19 @@ namespace bsread {
         uint64_t nsec;
     };
 
-    enum bsdata_type {
-            BSDATA_STRING,
-            BSDATA_BOOl,
-            BSDATA_FLOAT64,
-            BSDATA_FLOAT32,
-            BSDATA_INT8,
-            BSDATA_UINT8,
-            BSDATA_INT16,
-            BSDATA_UINT16,
-            BSDATA_INT32,
-            BSDATA_UINT32,
-            BSDATA_INT64,
-            BSDATA_UINT64};
-
-    static const size_t bsdata_type_size[] = {
-            1,
-            1,
-            8,
-            4,
-            1,
-            1,
-            2,
-            2,
-            4,
-            4,
-            8,
-            8
-    };
-
-    static const char* const bsdata_type_name[] = {
-            "string",
-            "bool",
-            "float64",
-            "float32",
-            "int8",
-            "uint8",
-            "int16",
-            "uint16",
-            "int32",
-            "uint32",
-            "int64",
-            "uint64"};
-
-    static const std::map<std::string, bsdata_type> bsdata_type_mapping = {
-            {"string", BSDATA_STRING},
-            {"bool", BSDATA_BOOl},
-            {"float64",BSDATA_FLOAT64},
-            {"float32",BSDATA_FLOAT32},
-            {"int8", BSDATA_INT8},
-            {"uint8", BSDATA_UINT8},
-            {"int16", BSDATA_INT16},
-            {"uint16", BSDATA_UINT16},
-            {"int32", BSDATA_INT32},
-            {"uint32", BSDATA_UINT32},
-            {"int64", BSDATA_INT64},
-            {"uint64", BSDATA_UINT64},
+    static const std::map<std::string, size_t> bsdata_type_n_bytes = {
+            {"string",  1},
+            {"bool",    1},
+            {"float64", 8},
+            {"float32", 4},
+            {"int8",    1},
+            {"uint8",   1},
+            {"int16",   2},
+            {"uint16",  2},
+            {"int32",   4},
+            {"uint32",  4},
+            {"int64",   8},
+            {"uint64",  8},
     };
 }
 
