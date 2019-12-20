ce048cef2a41600e8c121491e938da0222db25eb
Andrej Babic <andrej.babic@psi.ch>
2019-12-20 12:19:59 +0100

    Improve data channel to fit receiver

--- src/ChannelData.h
+++ src/ChannelData.h
@@ -9,15 +9,19 @@ namespace bs_daq {
 
     struct ChannelData {
 
+        // Attributes set on data header change.
         const std::string channel_name_;
         const int64_t pulse_id_mod_;
-        const int64_t pulse_id_;
         const std::string type_;
         const std::vector<uint32_t> shape_;
         const std::string encoding_;
         const std::string compression_;
-        const size_t data_n_bytes_;
+        const size_t buffer_n_bytes_;
         const std::unique_ptr<char> buffer_;
+
+        // Attributes set for each received message.
+        int64_t pulse_id_ = 0;
+        size_t data_n_bytes_ = 0;
 
         ChannelData(
                 std::string channel_name,
@@ -27,16 +31,15 @@ namespace bs_daq {
                 const std::vector<uint32_t>& shape,
                 std::string encoding,
                 std::string compression,
-                size_t data_n_bytes)
+                size_t buffer_n_bytes)
                 : channel_name_(channel_name),
                   pulse_id_mod_(pulse_id_mod),
-                  pulse_id_(pulse_id),
                   type_(type),
                   shape_(shape),
                   encoding_(encoding),
                   compression_(compression),
-                  data_n_bytes_(data_n_bytes),
-                  buffer_(std::make_unique<char>(data_n_bytes)) {}
+                  buffer_n_bytes_(buffer_n_bytes),
+                  buffer_(std::make_unique<char>(buffer_n_bytes)) {}
     };
 
 }
