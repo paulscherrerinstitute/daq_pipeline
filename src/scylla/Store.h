824a72a883c8f6ca86537025068c85db9dcf6f9c
Andrej Babic <andrej.babic@ringlspil.com>
2020-01-09 19:06:16 +0100

    Add insert statement to header

--- src/scylla/Store.h
+++ src/scylla/Store.h
@@ -6,12 +6,20 @@
 #include "../IDataStore.h"
 
 namespace scylla {
+
+    static const std::string INSERT_STATEMENT(
+        "INSERT INTO bsread_data.channel_data "
+        "(channel_name, pulse_id_mod, pulse_id, data, type, shape, encoding, compression) "
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
+
+    template<class T> using cass_ptr = std::unique_ptr<T, void (*)(T *)>;
+
     class Store : public bs_daq::IDataStore {
 
     protected:
-        CassCluster* cluster_ = NULL;
-        CassSession* session_ = NULL;
-        CassPrepared* prepared_insert_ = NULL;
+        cass_ptr<CassCluster> cluster_ = {NULL, NULL};
+        cass_ptr<CassSession> session_ = {NULL, NULL};
+        cass_ptr<const CassPrepared> prepared_insert_ = {NULL, NULL};
 
         void free_class_member_cass_objects();
 
