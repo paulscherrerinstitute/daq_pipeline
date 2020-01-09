62a790721a753465294efa756a80f8aae2bec4d4
Andrej Babic <andrej.babic@ringlspil.com>
2020-01-09 19:15:59 +0100

    Add pulse_id_mod_ to ChannelData constructor

--- src/ChannelData.h
+++ src/ChannelData.h
@@ -17,6 +17,7 @@ namespace bs_daq {
         const std::string compression_;
         const size_t buffer_n_bytes_;
         const std::unique_ptr<char> buffer_;
+        const int64_t pulse_id_mod_;
 
         // Attributes set for each received message.
         int64_t pulse_id_ = 0;
@@ -28,14 +29,16 @@ namespace bs_daq {
                 const std::vector<uint32_t>& shape,
                 std::string encoding,
                 std::string compression,
-                size_t buffer_n_bytes)
+                size_t buffer_n_bytes,
+                int64_t pulse_id_mod)
                 : channel_name_(channel_name),
                   type_(type),
                   shape_(shape),
                   encoding_(encoding),
                   compression_(compression),
                   buffer_n_bytes_(buffer_n_bytes),
-                  buffer_(std::make_unique<char>(buffer_n_bytes)) {}
+                  buffer_(std::make_unique<char>(buffer_n_bytes)),
+                  pulse_id_mod_(pulse_id_mod) {}
     };
 
 }
