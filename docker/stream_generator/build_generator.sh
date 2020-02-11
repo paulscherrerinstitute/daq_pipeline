2c5e5cc6f5f2f4d294d604a45c0f34f4deb98c63
Andrej Babic <andrej.babic@ringlspil.com>
2020-02-11 15:36:49 +0100

    Add stream generator sources

---
+++ docker/stream_generator/build_generator.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+VERSION=0.0.1
+
+cp ../../operational_tools/sources_all.json sources.json
+
+docker login
+
+docker build -f Dockerfile_generator --no-cache=true -t paulscherrerinstitute/daq_pipeline_generator .
+docker tag paulscherrerinstitute/daq_pipeline_generator paulscherrerinstitute/daq_pipeline_generator:$VERSION
+
+rm sources.json
+
+docker push paulscherrerinstitute/daq_pipeline_generator:$VERSION
+docker push paulscherrerinstitute/daq_pipeline_generator
