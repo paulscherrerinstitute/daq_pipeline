72a2ace7085abcea0b8c9b187f86d2a719f2214a
Andrej Babic <andrej.babic@ringlspil.com>
2020-02-13 09:56:49 +0100

    Improve the source generator with generators templates
    We split the daq into 2 parts:
    - Generator
    - Pipeline
    
    We now need to generate the files for the generators to be
    deployed on Kubernetes as well.

--- operational_tools/gen_sources.py
+++ operational_tools/gen_sources.py
@@ -4,33 +4,59 @@ import os
 import shutil
 from copy import deepcopy
 
+START_STREAM_PORT = 10000
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--source_file', type=str, default="sources_all.json",
-                    help='Simulated device name')
+                    help='File with the desired sources.')
+parser.add_argument('--scylla_hosts', type=str, default="",
+                    help='Scylla hosts to access the data.')
+parser.add_argument('--generator_host', type=str, default="",
+                    help='Host where the generator code will be running.')
 
 args = parser.parse_args()
 
-if os.path.exists("sources/"):
-    shutil.rmtree("sources/")
+if os.path.exists("daq_pipelines/"):
+    shutil.rmtree("daq_pipelines/")
 
-os.mkdir("sources/")
+if os.path.exists("generators/"):
+    shutil.rmtree("generators/")
+
+os.mkdir("daq_pipelines/")
+os.mkdir("generators/")
 
 with open("template_daq_pipeline.json") as input_file:
-    template = json.load(input_file)
+    template_daq_pipeline = json.load(input_file)
+
+with open("template_generator.json") as input_file:
+    template_generator = json.load(input_file)
 
 with open(args.source_file) as input_file:
     data = json.load(input_file)
 
-for device in data.keys():
-    device_file = deepcopy(template)
-    device_file["metadata"]["name"] = device.lower()
+for index, device in enumerate(data.keys()):
+    daq_pipeline_file = deepcopy(template_daq_pipeline)
+    generators_file = deepcopy(template_generator)
 
-    device_file["spec"]["containers"][0]["env"].append(
-        {
-            "name": "DEVICE_NAME",
-            "value": device
-        })
+    stream_port = START_STREAM_PORT + index
+    stream_address = "tcp://%s:%s" % (args.generator_host, stream_port)
 
-    with open("sources/%s.json" % device, 'w') as output_file:
-        json.dump(device_file, output_file, indent=4)
+    daq_pipeline_file["metadata"]["name"] = device.lower()
+    generators_file["metadata"]["name"] = device.lower()
+
+    daq_pipeline_file["spec"]["containers"][0]["env"]\
+        .append({"name": "SCYLLA_HOSTS", "value": args.scylla_hosts})
+    daq_pipeline_file["spec"]["containers"][0]["env"] \
+        .append({"name": "STREAM_HOST", "value": stream_address})
+
+    generators_file["spec"]["containers"][0]["env"] \
+        .append({"name": "DEVICE_NAME", "value": device})
+    generators_file["spec"]["containers"][0]["env"] \
+        .append({"name": "PORT", "value": stream_port})
+
+    with open("daq_pipelines/%s.json" % device, 'w') as output_file:
+        json.dump(daq_pipeline_file, output_file, indent=4)
+
+    with open("generators/%s.json" % device, 'w') as output_file:
+        json.dump(generators_file, output_file, indent=4)
