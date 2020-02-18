d26f1a9eb3faa566f6601fa18759e2246724cac9
Andrej Babic <andrej.babic@ringlspil.com>
2020-02-18 10:49:16 +0100

    Push docker container

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -5,7 +5,7 @@ VERSION=0.0.1
 docker login
 
 docker build -f docker/Dockerfile --no-cache=true -t paulscherrerinstitute/daq_pipeline .
-#docker tag paulscherrerinstitute/daq_pipeline_base paulscherrerinstitute/daq_pipeline:$VERSION
+docker tag paulscherrerinstitute/daq_pipeline_base paulscherrerinstitute/daq_pipeline:$VERSION
 
-#docker push paulscherrerinstitute/daq_pipeline:$VERSION
-#docker push paulscherrerinstitute/daq_pipeline
+docker push paulscherrerinstitute/daq_pipeline:$VERSION
+docker push paulscherrerinstitute/daq_pipeline
