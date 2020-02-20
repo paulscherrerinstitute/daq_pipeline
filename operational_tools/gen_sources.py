adb72ab9e5558f75c23e290ddf03db63f5e7cb5c
Andrej Babic <andrej.babic@ringlspil.com>
2020-02-20 14:14:40 +0100

    Improve source generator

--- operational_tools/gen_sources.py
+++ operational_tools/gen_sources.py
@@ -3,9 +3,12 @@ import json
 import os
 import shutil
 from copy import deepcopy
+from collections import defaultdict
 
 START_STREAM_PORT = 10000
 DEFAULT_SCYLLA_HOSTS = "172.26.120.72,172.26.120.73,172.26.120.74"
+
+GENERATOR_HOSTS = ["sf-daqstress-01", "sf-daqstress-02"]
 
 parser = argparse.ArgumentParser()
 
@@ -13,8 +16,6 @@ parser.add_argument('--source_file', type=str, default="sources/sources_all.json
                     help='File with the desired sources.')
 parser.add_argument('--scylla_hosts', type=str, default=DEFAULT_SCYLLA_HOSTS,
                     help='Scylla hosts to access the data.')
-parser.add_argument('--generator_host', type=str, default="172.26.120.71",
-                    help='Host where the generator code will be running.')
 
 args = parser.parse_args()
 
@@ -36,15 +37,17 @@ with open("template_generator.json") as input_file:
 with open(args.source_file) as input_file:
     data = json.load(input_file)
 
-docker_command = 'docker run --net=host -e DEVICE_NAME="%s" -e PORT=%s paulscherrerinstitute/daq_pipeline_generator &'
-temp = []
+docker_command = ' run --net=host -e DEVICE_NAME="%s" -e PORT=%s paulscherrerinstitute/daq_pipeline_generator &'
+bash_generators = defaultdict(list)
 
 for index, device in enumerate(data.keys()):
     daq_pipeline_file = deepcopy(template_daq_pipeline)
     generators_file = deepcopy(template_generator)
 
+    generator_host = GENERATOR_HOSTS[index % len(GENERATOR_HOSTS)]
+
     stream_port = START_STREAM_PORT + index
-    stream_address = "tcp://%s:%s" % (args.generator_host, stream_port)
+    stream_address = "tcp://%s:%s" % (generator_host, stream_port)
 
     daq_pipeline_file["metadata"]["name"] = device.lower()
     generators_file["metadata"]["name"] = device.lower()
@@ -59,7 +62,7 @@ for index, device in enumerate(data.keys()):
     generators_file["spec"]["containers"][0]["env"] \
         .append({"name": "PORT", "value": stream_port})
 
-    temp.append((device,docker_command % (device, stream_port)))
+    bash_generators[generator_host].append((device,docker_command % (device, stream_port)))
 
     with open("daq_pipelines/%s.json" % device, 'w') as output_file:
         json.dump(daq_pipeline_file, output_file, indent=4)
@@ -67,6 +70,9 @@ for index, device in enumerate(data.keys()):
     with open("generators/%s.json" % device, 'w') as output_file:
         json.dump(generators_file, output_file, indent=4)
 
-temp = sorted(temp, key=lambda x:x[0])
-with open("generators/start_generators.sh", 'w') as output_file:
-    output_file.write("\n".join((x[1] for x in temp)))
+for key, value in bash_generators.items():
+
+    temp = sorted(value, key=lambda x:x[0])
+
+    with open("generators/%s.sh" % key, 'w') as output_file:
+        output_file.write("\n".join((x[1] for x in temp)))
