e59085d6b5dc43bddee124b333e816cbb3b4f2d5
Andrej Babic <andrej.babic@psi.ch>
2020-02-22 10:00:02 +0100

    Fix statistics calculations

--- src/stats/ConsoleStats.cpp
+++ src/stats/ConsoleStats.cpp
@@ -8,13 +8,13 @@ void debug::ConsoleStats::add_stats(uint64_t pulse_id, const bs_daq::StatsData& 
     if (stats_.n_pulses_ == 100) {
         std::cout << "pulse_id="<< pulse_id << " {";
 	
-	std::cout << "\"iteration\": " << stats_.iteration_ << ", ";
-	std::cout << "\"get_data\": " << stats_.get_data_ << ", ";
-	std::cout << "\"save_data\": " << stats_.save_data_ << ", ";
-	std::cout << "\"add_stats\": " << stats_.add_stats_ << ", ";
-	std::cout << "\"n_data_bytes\": " << stats_.n_data_bytes_ << ", ";
-	std::cout << "\"n_pending_inserts\": " << stats_.n_pending_inserts_ << ", ";
-	std::cout << "\"n_pulses\": " << stats_.n_pulses_;
+	std::cout << "\"iteration\": " << stats_.iteration_/100 << ", ";
+	std::cout << "\"get_data\": " << stats_.get_data_/100<< ", ";
+	std::cout << "\"save_data\": " << stats_.save_data_/100<< ", ";
+	std::cout << "\"add_stats\": " << stats_.add_stats_/100<< ", ";
+	std::cout << "\"n_data_bytes\": " << stats_.n_data_bytes_/100<< ", ";
+	std::cout << "\"n_pending_inserts\": " << stats_.n_pending_inserts_/100<< ", ";
+	std::cout << "\"n_pulses\": " << stats_.n_pulses_/100;
         
 	std::cout << "}" << std::endl;
 
