b488af6e69174f0838d434b110e3662964d0631d
Andrej Babic <andrej.babic@ringlspil.com>
2020-02-26 10:08:32 +0100

    Optimize statistics output

--- src/stats/ConsoleStats.cpp
+++ src/stats/ConsoleStats.cpp
@@ -2,19 +2,23 @@
 #include <iomanip>
 #include "ConsoleStats.h"
 
+debug::ConsoleStats::ConsoleStats(const std::string& device_name, size_t n_pulses_output):
+    device_name_(device_name),
+    n_pulses_output_(n_pulses_output)
+{}
+
 void debug::ConsoleStats::add_stats(uint64_t pulse_id, const bs_daq::StatsData& pulse_stats) {
     BaseStats::add_stats(pulse_id, pulse_stats);
 
-    if (stats_.n_pulses_ == 100) {
-        std::cout << "pulse_id="<< pulse_id << " {";
-	
-        std::cout << "\"iteration\": " << stats_.iteration_/100 << ", ";
-        std::cout << "\"get_data\": " << stats_.get_data_/100<< ", ";
-        std::cout << "\"n_data_bytes\": " << stats_.n_data_bytes_/100<< ", ";
-        std::cout << "\"n_pending_inserts\": " << stats_.n_pending_inserts_/100<< ", ";
-        std::cout << "\"n_pulses\": " << stats_.n_pulses_/100;
+    std::cout << std::fixed << std::setprecision( 2 );
 
-        std::cout << "}" << std::endl;
+    if (stats_.n_pulses_ == n_pulses_output_) {
+        std::cout << device_name_ << " ";
+        std::cout << "i: " << stats_.iteration_/n_pulses_output_*1000 << " ";
+        std::cout << "d: " << stats_.get_data_/n_pulses_output_*1000<< " ";
+        std::cout << "b: " << stats_.n_data_bytes_/n_pulses_output_<< " ";
+        std::cout << "np: " << stats_.n_pending_inserts_/n_pulses_output_;
+        std::cout << std::endl;
 
         stats_ = bs_daq::StatsData();
     }
--- src/stats/ConsoleStats.h
+++ src/stats/ConsoleStats.h
@@ -6,7 +6,11 @@
 
 namespace debug {
     class ConsoleStats : public bs_daq::BaseStats {
+        const std::string device_name_;
+        const size_t n_pulses_output_;
+
     public:
+        ConsoleStats(const std::string& device_name, size_t n_pulses_output);
         void add_stats(uint64_t pulse_id, const bs_daq::StatsData& pulse_stats);
     };
 }
