4051f27c694d95c1c50443b1ac0c1b43529a4caa
Andrej Babic <andrej.babic@ringlspil.com>
2020-02-26 11:14:24 +0100

    Move output setting to constructor

--- src/stats/ConsoleStats.cpp
+++ src/stats/ConsoleStats.cpp
@@ -5,13 +5,13 @@
 debug::ConsoleStats::ConsoleStats(const std::string& device_name, size_t n_pulses_output):
     device_name_(device_name),
     n_pulses_output_(n_pulses_output)
-{}
+{
+    std::cout << std::fixed << std::setfill('0');
+    std::cout << std::setw( 5 ) << std::setprecision( 3 );
+}
 
 void debug::ConsoleStats::add_stats(uint64_t pulse_id, const bs_daq::StatsData& pulse_stats) {
     BaseStats::add_stats(pulse_id, pulse_stats);
-
-    std::cout << std::fixed << std::setfill('0');
-    std::cout << std::setw( 5 ) << std::setprecision( 3 );
 
     if (stats_.n_pulses_ == n_pulses_output_) {
         std::cout << device_name_ << " ";
